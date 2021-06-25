3da8cf6f5a7730ab4f7cef9b2219a519dfa9af85
kadu <kadu@kadu.com.br>
2021-06-24 21:08:27 -0300

    Testes das funções Básicas (Led, botao, timer)

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,56 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+#include <OneButton.h>
+#include <SimpleTimer.h>
+
+int tempo = 30;
+
+SimpleTimer timer;
+OneButton button1(D1, false);
+Adafruit_NeoPixel pixels(72, 2, NEO_GRB + NEO_KHZ800);
+
+void btnStartPause() {
+  Serial.println("Play / Pause");
+  pixels.clear();
+  pixels.setPixelColor(20, pixels.Color(255,0, 0));
+  pixels.show();
+}
+
+void btnStop_StartPress() {
+  Serial.println("Play / Pause");
+  pixels.clear();
+  pixels.setPixelColor(20, pixels.Color(0,255, 0));
+  pixels.show();
+}
+
+void btnStop() {
+  Serial.println("Stop Pressed");
+  pixels.clear();
+  pixels.setPixelColor(20, pixels.Color(0,0, 255));
+  pixels.show();
+}
+
+void repeatMe() {
+    Serial.print("Uptime (s): ");
+    Serial.println(millis() / 1000);
+}
+
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pixels.begin();
+  pixels.setBrightness(15);
+
+  button1.attachClick(btnStartPause);
+  button1.attachLongPressStart(btnStop_StartPress);
+  button1.attachLongPressStop(btnStop);
+
+  pixels.clear();
+  pixels.show();
+  timer.setInterval(1000, repeatMe);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  timer.run();
+  button1.tick();
 }
