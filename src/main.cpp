ac4b677f4f8661d73b765e30b81bb2a1133523b8
kadu <kadu@kadu.com.br>
2021-06-29 19:59:51 -0300

    Limpando código, para usar classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,53 +1,72 @@
 #include <Arduino.h>
-#include <Adafruit_NeoPixel.h>
 #include <OneButton.h>
 #include <SimpleTimer.h>
+#include <pomodoro.h>
 
-int tempo = 30;
+#define POMODOROINTERVAL    5
+#define POMODOROTIMER       25
+#define POMODOROBIGINTERVAL 15
+#define _MINUTE_IN_MS       1000
+
+unsigned int contapomodoro = 0;
+byte LEDCOUNT = 71;
+byte NEOPIXELPIN=2;
 
 SimpleTimer timer;
 OneButton button1(D1, false);
-Adafruit_NeoPixel pixels(72, 2, NEO_GRB + NEO_KHZ800);
+Pomodoro pomodoro(NEOPIXELPIN, LEDCOUNT, 5, 15, 25);
 
 void btnStartPause() {
   Serial.println("Play / Pause");
-  pixels.clear();
-  pixels.setPixelColor(20, pixels.Color(255,0, 0));
-  pixels.show();
+  pomodoro.start();
+
+  contapomodoro = 25;
 }
 
 void btnStop_StartPress() {
   Serial.println("Play / Pause");
-  pixels.clear();
-  pixels.setPixelColor(20, pixels.Color(0,255, 0));
-  pixels.show();
 }
 
 void btnStop() {
   Serial.println("Stop Pressed");
-  pixels.clear();
-  pixels.setPixelColor(20, pixels.Color(0,0, 255));
-  pixels.show();
 }
 
-void repeatMe() {
-    Serial.print("Uptime (s): ");
-    Serial.println(millis() / 1000);
+void minutosTick() {
+  if(contapomodoro > 0) {
+    contapomodoro--;
+  }
+
+  // pixels.clear();
+  // uint32_t cor;
+  // int mapa = map(contapomodoro, 25,0,LEDCOUNT,0);
+
+  // if(contapomodoro > 5) {
+  //   cor = azul;
+  // } else {
+  //   cor = vermelho;
+  // }
+
+  // for (int i = 0; i < mapa; i++) {
+  //   pixels.setPixelColor(i,cor);
+  // }
+  // pixels.show();
 }
 
+void debugSegundo() {
+  Serial.println(contapomodoro);
+}
 
 void setup() {
   Serial.begin(115200);
-  pixels.begin();
-  pixels.setBrightness(15);
+
 
   button1.attachClick(btnStartPause);
   button1.attachLongPressStart(btnStop_StartPress);
   button1.attachLongPressStop(btnStop);
 
-  pixels.clear();
-  pixels.show();
-  timer.setInterval(1000, repeatMe);
+
+  timer.setInterval(_MINUTE_IN_MS, minutosTick);
+  timer.setInterval(1000, debugSegundo);
 }
 
 void loop() {
